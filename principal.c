c72b227c7aa5ffa4df8bfdd96c62a2d0df2dbdfd
nikasha89 <nikasha_moi@hotmail.com>
2016-04-27 19:21:10 +0200

    Eliminando Listas para Hacerlo forma 1: Array Bidimensional

--- principal.c
+++ principal.c
@@ -1,65 +1,35 @@
 #include "hdr.h"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
-/* Recogemos los parametros de ancho, alto y lugar opcion */
+	/* Recogemos los parametros de ancho, alto y lugar opcion */
 
-//Ancho y Alto del mundo:
-int width, high, c;
-/* Cadena de caracter que guarda el path del archivo que contiene
-la sesion anteriormente jugada y guardada del mundo */
-char *directorio;
+	//Ancho y Alto del mundo:
+	int width, high, c;
+	/* Cadena de caracter que guarda el path del archivo que contiene
+	la sesion anteriormente jugada y guardada del mundo */
+	char *directorio;
 
-while((c = getopt( argc, argv, "w:h:d::"))!=-1){
-	switch(c){
-		case 'w':
-			width = strtol(optarg, NULL, 0);
-			break;
-		case 'h':
-			high = strtol(optarg, NULL, 0);
-			break;
-		case 'd':
-			directorio = optarg;
-			break;
-		default:
-			return EXIT_FAILURE;
+	while((c = getopt( argc, argv, "w:h:d::"))!=-1){
+		switch(c){
+			case 'w':
+				width = strtol(optarg, NULL, 0);
+				break;
+			case 'h':
+				high = strtol(optarg, NULL, 0);
+				break;
+			case 'd':
+				directorio = optarg;
+				break;
+			default:
+				return EXIT_FAILURE;
+		}
 	}
-}
+	printf("width = %d; high= %d; directorio:%s \n", width, high, directorio);
 
-printf("width = %d; high= %d; directorio:%s \n", width, high, directorio);
-/*
-	for(int i=stdin; i < argc;i++){
-		printf( "Esto sobra(argumento desconocido): %s", argv[i] );
-	}
-
-	enum estado_celula{
-		VIVA, 
-		MUERTA
-	} EstadoCelula;
-
-	Inicializamos célula con el estado MUERTA por defecto 
 	struct celula{
-		enum estado_celula estado;
-	} Celula;
-*/
-	struct lista{
-		/* Aunque sean de tipo int, lo declararemos como unsigned char
-		de esta forma ocupa menos espacio en memoria
-		*/
-		unsigned char x; 
-		unsigned char y;
-		int *next;
-		int *prev;
-	};
-
-	struct tablero{
-		int iteracion;
-		struct coordenadas arrayCelulasVivas[TAM_ARRAY*TAM_ARRAY];
-		int indexVivas;
-		struct coordenadas arrayCelulasNacen[TAM_ARRAY*TAM_ARRAY];
-		int indexNacidas;
-		struct coordenadas arrayCelulasMueren[TAM_ARRAY*TAM_ARRAY];
-		int indexMuertas;
+		bool muerta;
 	} Tablero;
 		int numeroCelulas = menuInicio();
 		printf("%d\n",numeroCelulas);	
